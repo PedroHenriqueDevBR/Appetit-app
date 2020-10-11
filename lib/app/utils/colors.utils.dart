7865436059878b54f5b2f0c402914391c5cfac94
PedroHenriqueDevBR <p.lyma18@gmail.com>
2020-10-10 21:11:57 -0300

    feat: create a color class to centralize app colors

---
+++ lib/app/utils/colors.utils.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  Color backgroundColor = Color(0XFFFAFAFA);
+  Color primaryColor = Color(0XFFFF8822);
+  Color primaryDarkColor = Color(0XFFE57AF1);
+  Color secondaryColor = Color(0XFFB8CC3B);
+}
