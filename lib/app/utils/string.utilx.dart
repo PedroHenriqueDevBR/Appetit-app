454bab598fa824bec8ffe109a3c16fa897209233
PedroHenriqueDevBR <p.lyma18@gmail.com>
2020-10-10 21:12:29 -0300

    feat: create a string class to centralize app informations

---
+++ lib/app/utils/string.utilx.dart
@@ -0,0 +1,37 @@
+class AppString {
+  String appName = 'Appéttt';
+  String wellcome = 'Seja bem-vindo!';
+  String wellcomeDescription = 'Nós sabemos a importância de estar sempre de barriga cheia e o quanto isso pode ajudar no seu dia.';
+  String emailInput = 'Insira o seu e-mail aqui';
+  String passwordInput = 'Insira a sua senha aqui';
+  String enterButton = 'ENTRAR';
+
+  String hello = 'Olá, ';
+  String newDemand = 'fazer novo pedido';
+  String search = 'Digite a sua busca aqui';
+  String saleDay = ', você já vendeu ';
+
+  String demandInfo = 'Informações para o pedido';
+  String fillFields = 'Preencha as informações abaixo para concluir o pedido.';
+  String whatSelling = 'O que você está vendendo?';
+
+  String demandDetails = 'Detalhes do pedido';
+  String demandDetailsDescription = 'caso queira, aproveite para adicionar alguma observação para este pedido.';
+  String options = 'Opções';
+  String optionsDescription = 'Escolha uma das opções de massas disponíveis abaixo.';
+  String observations = 'Observações';
+  String add = 'Adicionar';
+  String whichClient = 'Para quem você está vendendo?';
+  String myClients = 'Para quem você está vendendo?';
+  String paindOrder = 'Para quem você está vendendo?';
+  String finalizeDemand = 'finalizazr pedido';
+
+  String yes = 'Sim';
+  String no = 'Não';
+  String whitchDate = 'Em que data o pedido foi realizado?';
+  String chooseDate = 'Selecione uma data';
+
+  String doneDemand = 'Pedido realizado!';
+  String goToHomePage = 'voltar para a página inicial';
+
+}
